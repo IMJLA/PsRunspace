19271d5551ca08d1d30c199203cf02b7a75dc158
Jeremy La Camera <IMJLA@users.noreply.github.com>
2022-07-30 08:36:20 -0700

    Updated debug output

--- dist/1.0.47/PsRunspace/PsRunspace.psm1
+++ dist/1.0.47/PsRunspace/PsRunspace.psm1
@@ -869,3 +869,4 @@ Export-ModuleMember -Function @('Add-PsCommand','Add-PsModule','Expand-PsCommand
 
 
 
+
--- src/PsRunspace.psm1
+++ src/PsRunspace.psm1
@@ -37,3 +37,4 @@ Export-ModuleMember -Function @('Add-PsCommand','Add-PsModule','Expand-PsCommand
 
 
 
+
